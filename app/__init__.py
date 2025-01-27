e4919fe9683e478d90bca63a3baf81fc37d2d4f2
Shirshak52 <np01cp4s230122@islingtoncollege.edu.np>
2025-01-27 15:21:59 +0545

    - implemented app factory function - index page shifted to 'main' blueprint folder - installed psycopg2-binary for PostgreSQL db connection - added/updated .env and config.py files

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,33 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from app.config import Config
 
-app = Flask(__name__)
+# Initialize addon instances
+db = SQLAlchemy()
+migrate = Migrate()
 
 
-@app.route("/")
-@app.route("/index")
-def index():
-    return render_template("index.html")
+def create_app():
+    # Create and configure the Flask app
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    with app.app_context():
+
+        # Import model classes for migration
+
+        # Setup admin views
+
+        # Import routes
+        from app.blueprints.main.routes import index
+
+        # Register blueprints
+        from app.blueprints.main import main_bp
+
+        app.register_blueprint(main_bp)
+
+    return app
---
+++ app/config.py
@@ -0,0 +1,21 @@
+from os import environ, path
+from dotenv import load_dotenv
+
+# Specify a '.env' file containing key/value config values
+basedir = path.abspath(path.join(path.dirname(__file__), ".."))
+load_dotenv(path.join(basedir, ".env"))
+# print(f"Loading .env from: {path.join(basedir, '.env')}")
+
+
+class Config:
+    """Set Flask application configuration variables."""
+
+    # General config values
+    ENVIRONMENT = environ.get("ENVIRONMENT")
+    FLASK_APP = environ.get("FLASK_APP")
+    FLASK_DEBUG = environ.get("FLASK_DEBUG")
+    SECRET_KEY = environ.get("SECRET_KEY")
+
+    # Database config values
+    SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_TRACK_MODIFICATIONS = environ.get("SQLALCHEMY_TRACK_MODIFICATIONS")

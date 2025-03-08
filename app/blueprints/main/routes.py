b8cc3601fb19170920f47eb531bd716082dbeb46
Shirshak52 <np01cp4s230122@islingtoncollege.edu.np>
2025-03-08 20:35:10 +0545

    removed login required for home page

--- app/blueprints/main/routes.py
+++ app/blueprints/main/routes.py
@@ -6,7 +6,6 @@ from app.blueprints.main import main_bp
 
 @main_bp.route("/")
 @main_bp.route("/index")
-@login_required
 def index():
     """Displays the home page."""
     return render_template("index.html")

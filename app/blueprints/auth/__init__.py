b8f8a61e8f31900627087c4ff6ab63f2a4a45b41
Shirshak52 <np01cp4s230122@islingtoncollege.edu.np>
2025-04-12 20:40:57 +0545

    ui change in login

--- app/blueprints/auth/__init__.py
+++ app/blueprints/auth/__init__.py
@@ -1,3 +1,9 @@
 from flask import Blueprint
 
-auth_bp = Blueprint("auth", __name__, template_folder="templates", url_prefix="/auth")
+auth_bp = Blueprint(
+    "auth",
+    __name__,
+    template_folder="templates",
+    static_folder="static",
+    url_prefix="/auth",
+)
